142127e3b5e0748f1208e75a5f9bf5178040d456
jake howlett <jake.howlett93@gmail.com>
2021-02-16 15:33:09 +0000

    add simple form gem and a form partial file rendered in new

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,11 +17,10 @@ class ArticlesController < ApplicationController
 
     redirect_to article_path(@article)
   end
-  
+
   private
 
   def article_params
   params.require(:article).permit(:title, :content)
   end
-
 end
